f9bf934be1ec1a1acf0a1cf3aaa32b3bf709de3d
irenejenifer <52645515+irenejenifer@users.noreply.github.com>
2019-07-16 12:35:20 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,11 +1,7 @@
-num=int(input())
-if num > 1: 
-   for i in range(2, num//2): 
-       if (num % i) == 0: 
-           print('no') 
-           break
-   else: 
-       print('yes') 
-  
-else: 
-   print('no') 
+a,b=map(int,input().split())
+for num in range(a+1,b):
+  for i in range(2,num):
+    if(num%i==0):
+      break
+  else:
+      print(num,end=" ")
